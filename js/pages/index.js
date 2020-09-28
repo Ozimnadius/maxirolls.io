33ad0ceb1576547fda801a30bebe7b991a635043
Mikle Seregin <mikhail-seregin@yandex.ru>
2020-09-28 17:57:36 +0300

    index,actions,about, contacts (html/css)

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,5 +1,24 @@
 //index.js
 $(function () {
 
+    let navSwiper = new Swiper('.hnav__container', {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+    });
+
+    $('.sw').on('click', function (e) {
+        e.preventDefault();
+
+        let $this = $(this),
+            sws = $('.sw'),
+            tabs = $('.tab'),
+            tab = $(tabs[sws.index($this)]);
+
+        sws.removeClass('active');
+        tabs.removeClass('active');
+        $this.addClass('active');
+        tab.addClass('active');
+
+    });
 
 });
